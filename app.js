99edad587f03cd91d401517a6d87cfb7771b14ae
Mitu Adrian <adrianmitu83@gmail.com>
2025-04-24 00:13:26 +0300

    Started Express Server with an index template

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("node:path");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Mini message board - listening on port ${PORT}!`);
+});
